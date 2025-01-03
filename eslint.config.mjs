d3c9c3d0234f7038fe70174e4fa440599a1dc2f8
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-02 20:28:20 -0500

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { ...globals.node, ...global.browser } },
+  pluginJs.configs.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      'prettier/prettier': 'error', // Treat Prettier formatting issues as errors
+      'capitalized-comments': ['error', 'always'],
+      eqeqeq: ['error', 'always'], // Enforce strict equality (===)
+      // 'no-console': 'warn', // Warn when console.log is used
+    },
+  },
+];
