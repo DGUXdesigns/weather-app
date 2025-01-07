a3a92f42513790dd43691932489b07c45e7c8eb5
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-06 22:33:39 -0500

    miscellaneous

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,15 +4,19 @@ import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { ...globals.node, ...global.browser } },
-  pluginJs.configs.recommended,
-  eslintPluginPrettier,
-  {
-    rules: {
-      'prettier/prettier': 'error', // Treat Prettier formatting issues as errors
-      'capitalized-comments': ['error', 'always'],
-      eqeqeq: ['error', 'always'], // Enforce strict equality (===)
-      // 'no-console': 'warn', // Warn when console.log is used
-    },
-  },
+	{
+		languageOptions: {
+			globals: { ...globals.node, ...globals.browser },
+		},
+	},
+	pluginJs.configs.recommended,
+	eslintPluginPrettier,
+	{
+		rules: {
+			'prettier/prettier': 'error', // Treat Prettier formatting issues as errors
+			'capitalized-comments': ['error', 'always'],
+			eqeqeq: ['error', 'always'], // Enforce strict equality (===)
+			'no-console': 'warn', // Warn when console.log is used
+		},
+	},
 ];
